99be084b89fb8c05a2cdd52a43c6f8aeb7d0bedf
Adam Shannon <adamkshannon@gmail.com>
2019-06-06 14:48:13 -0500

    cmd/server: make basic package to fix compile
    `make docker` was failing because there was no cmd/server package, so
    let's add something quick.
    
    $ docker run moov/imagecashletter:v0.1.0-dev
    Starting moov-io/imagecashletter version v0.1.0-dev

---
+++ cmd/server/main.go
@@ -0,0 +1,15 @@
+// Copyright 2019 The Moov Authors
+// Use of this source code is governed by an Apache License
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"fmt"
+
+	"github.com/moov-io/imagecashletter"
+)
+
+func main() {
+	fmt.Printf("Starting moov-io/imagecashletter version %s\n", imagecashletter.Version)
+}
