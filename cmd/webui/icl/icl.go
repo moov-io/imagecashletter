8f8cf49bfcc18a636acfd390c672449ec52903cd
Adam Shannon <adamkshannon@gmail.com>
2020-08-14 18:31:41 -0500

    cmd/webui: include ICL file parsing

--- cmd/webui/icl/icl.go
+++ cmd/webui/icl/icl.go
@@ -5,10 +5,28 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
+	"strings"
 	"syscall/js"
+
+	"github.com/moov-io/imagecashletter"
 )
+
+func parseContents(input string) (string, error) {
+	r := strings.NewReader(input)
+	file, err := imagecashletter.NewReader(r).Read()
+	if err != nil {
+		return "", err
+	}
+
+	var buf bytes.Buffer
+	if err := json.NewEncoder(&buf).Encode(file); err != nil {
+		return "", err
+	}
+	return buf.String(), nil
+}
 
 func prettyJson(input string) (string, error) {
 	var raw interface{}
@@ -27,20 +45,26 @@ func jsonWrapper() js.Func {
 		if len(args) != 1 {
 			return "Invalid no of arguments passed"
 		}
+
 		inputJSON := args[0].String()
-		fmt.Printf("input %s\n", inputJSON)
-		pretty, err := prettyJson(inputJSON)
+
+		parsed, err := parseContents(inputJSON)
 		if err != nil {
 			fmt.Printf("unable to convert to json %s\n", err)
 			return err.Error()
 		}
+		pretty, err := prettyJson(parsed)
+		if err != nil {
+			fmt.Printf("unable to convert to json %s\n", err)
+			return err.Error()
+		}
+
 		return pretty
 	})
 	return jsonFunc
 }
 
 func main() {
-	// fmt.Println("Go Web Assembly")
-	js.Global().Set("formatJSON", jsonWrapper())
+	js.Global().Set("parseContents", jsonWrapper())
 	<-make(chan bool)
 }
