a5c454ac24c2bda909ea47953c3164166040309b
Sara Adineh <saraa@remitly.com>
2021-10-25 14:29:42 -0700

    Make BOFDItemSequenceNumber conditional

--- returnDetailAddendumA_test.go
+++ returnDetailAddendumA_test.go
@@ -28,6 +28,22 @@ func mockReturnDetailAddendumA() ReturnDetailAddendumA {
 	return rdAddendumA
 }
 
+// mockReturnDetailAddendumAWithoutBOFDItemSequenceNumber creates a ReturnDetailAddendumA
+func mockReturnDetailAddendumAWithoutBOFDItemSequenceNumber() ReturnDetailAddendumA {
+	rdAddendumA := NewReturnDetailAddendumA()
+	rdAddendumA.RecordNumber = 1
+	rdAddendumA.ReturnLocationRoutingNumber = "121042882"
+	rdAddendumA.BOFDEndorsementDate = time.Now()
+	rdAddendumA.BOFDAccountNumber = "938383"
+	rdAddendumA.BOFDBranchCode = "01"
+	rdAddendumA.PayeeName = "Test Payee"
+	rdAddendumA.TruncationIndicator = "Y"
+	rdAddendumA.BOFDConversionIndicator = "1"
+	rdAddendumA.BOFDCorrectionIndicator = 0
+	rdAddendumA.UserField = ""
+	return rdAddendumA
+}
+
 func TestReturnDetailAddendumAParseErr(t *testing.T) {
 	var r ReturnDetailAddendumA
 	r.Parse("asdlsajhfakjfa")
@@ -74,6 +90,105 @@ func TestMockReturnDetailAddendumA(t *testing.T) {
 	}
 	if rdAddendumA.UserField != "" {
 		t.Error("UserField does not validate")
+	}
+}
+
+// TestMockReturnDetailAddendumA creates a ReturnDetailAddendumA
+func TestMockReturnDetailAddendumAWithoutBOFDItemSequenceNumber(t *testing.T) {
+	rdAddendumA := mockReturnDetailAddendumAWithoutBOFDItemSequenceNumber()
+	if err := rdAddendumA.Validate(); err != nil {
+		t.Error("mockReturnDetailAddendumA does not validate and will break other tests: ", err)
+	}
+	if rdAddendumA.recordType != "32" {
+		t.Error("recordType RecordNumber does not validate")
+	}
+	if rdAddendumA.RecordNumber != 1 {
+		t.Error("RecordNumber does not validate")
+	}
+	if rdAddendumA.ReturnLocationRoutingNumber != "121042882" {
+		t.Error("ReturnLocationRoutingNumber does not validate")
+	}
+	if rdAddendumA.BOFDAccountNumber != "938383" {
+		t.Error("BOFDAccountNumber does not validate")
+	}
+	if rdAddendumA.BOFDBranchCode != "01" {
+		t.Error("BOFDBranchCode does not validate")
+	}
+	if rdAddendumA.PayeeName != "Test Payee" {
+		t.Error("PayeeName does not validate")
+	}
+	if rdAddendumA.TruncationIndicator != "Y" {
+		t.Error("TruncationIndicator does not validate")
+	}
+	if rdAddendumA.BOFDConversionIndicator != "1" {
+		t.Error("BOFDConversionIndicator does not validate")
+	}
+	if rdAddendumA.BOFDCorrectionIndicator != 0 {
+		t.Error("BOFDCorrectionIndicator does not validate")
+	}
+	if rdAddendumA.UserField != "" {
+		t.Error("UserField does not validate")
+	}
+}
+
+// TestParseReturnDetailAddendumAWithoutBOFDItemSequenceNumber validates parsing a ReturnDetailAddendumA
+func TestParseReturnDetailAddendumAWithoutBOFDItemSequenceNumber(t *testing.T) {
+	var line = "32112104288220180905               938383            01   Test Payee     Y10    "
+	r := NewReader(strings.NewReader(line))
+	r.line = line
+	clh := mockCashLetterHeader()
+	r.addCurrentCashLetter(NewCashLetter(clh))
+	bh := mockBundleHeader()
+	rb := NewBundle(bh)
+	r.currentCashLetter.AddBundle(rb)
+	r.addCurrentBundle(rb)
+	rd := mockReturnDetail()
+	r.currentCashLetter.currentBundle.AddReturnDetail(rd)
+
+	if err := r.parseReturnDetailAddendumA(); err != nil {
+		t.Errorf("%T: %s", err, err)
+		log.Fatal(err)
+	}
+	record := r.currentCashLetter.currentBundle.GetReturns()[0].ReturnDetailAddendumA[0]
+
+	if record.recordType != "32" {
+		t.Errorf("RecordType Expected '32' got: %v", record.recordType)
+	}
+	if record.RecordNumberField() != "1" {
+		t.Errorf("RecordNumber Expected '1' got: %v", record.RecordNumberField())
+	}
+	if record.ReturnLocationRoutingNumberField() != "121042882" {
+		t.Errorf("ReturnLocationRoutingNumber Expected '121042882' got: %v", record.ReturnLocationRoutingNumberField())
+	}
+	if record.BOFDEndorsementDateField() != "20180905" {
+		t.Errorf("BOFDEndorsementDate Expected '20180905' got: %v", record.BOFDEndorsementDateField())
+	}
+	if record.BOFDItemSequenceNumberField() != "               " {
+		t.Errorf("BOFDItemSequenceNumber Expected '                ' got: %v", record.BOFDItemSequenceNumberField())
+	}
+	if record.BOFDAccountNumberField() != "938383            " {
+		t.Errorf("BOFDAccountNumber Expected '938383            ' got: %v", record.BOFDAccountNumberField())
+	}
+	if record.BOFDBranchCodeField() != "01   " {
+		t.Errorf("BOFDBranchCode Expected '01   ' got: %v", record.BOFDBranchCodeField())
+	}
+	if record.PayeeNameField() != "Test Payee     " {
+		t.Errorf("PayeeName Expected 'Test Payee     ' got: %v", record.PayeeNameField())
+	}
+	if record.TruncationIndicatorField() != "Y" {
+		t.Errorf("TruncationIndicator Expected 'Y' got: %v", record.TruncationIndicatorField())
+	}
+	if record.BOFDConversionIndicatorField() != "1" {
+		t.Errorf("BOFDConversionIndicator Expected '1' got: %v", record.BOFDConversionIndicatorField())
+	}
+	if record.BOFDCorrectionIndicatorField() != "0" {
+		t.Errorf("BOFDCorrectionIndicator Expected '0' got: %v", record.BOFDCorrectionIndicatorField())
+	}
+	if record.UserFieldField() != " " {
+		t.Errorf("UserField Expected ' ' got: %v", record.UserFieldField())
+	}
+	if record.reservedField() != "   " {
+		t.Errorf("reserved Expected '   ' got: %v", record.reservedField())
 	}
 }
 
@@ -357,19 +472,6 @@ func TestRDAddendumAFIBOFDEndorsementDate(t *testing.T) {
 	if err := rdAddendumA.Validate(); err != nil {
 		if e, ok := err.(*FieldError); ok {
 			if e.FieldName != "BOFDEndorsementDate" {
-				t.Errorf("%T: %s", err, err)
-			}
-		}
-	}
-}
-
-// TestRDAddendumAFIBOFDItemSequenceNumber validation
-func TestRDAddendumAFIBOFDItemSequenceNumber(t *testing.T) {
-	rdAddendumA := mockReturnDetailAddendumA()
-	rdAddendumA.BOFDItemSequenceNumber = "               "
-	if err := rdAddendumA.Validate(); err != nil {
-		if e, ok := err.(*FieldError); ok {
-			if e.FieldName != "BOFDItemSequenceNumber" {
 				t.Errorf("%T: %s", err, err)
 			}
 		}
