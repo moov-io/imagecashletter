05a2641fe8e153c2e8e9bc47f740b58fa64d6463
Adam Shannon <adamkshannon@gmail.com>
2022-01-18 14:26:13 -0600

    cmd/webui: pass a data URI through instead of raw file contents

--- cmd/webui/icl/icl_js.go
+++ cmd/webui/icl/icl_js.go
@@ -6,26 +6,45 @@ package main
 
 import (
 	"bytes"
+	"encoding/base64"
 	"encoding/json"
 	"fmt"
 	"strings"
 	"syscall/js"
 
 	"github.com/moov-io/imagecashletter"
+
+	"github.com/vincent-petithory/dataurl"
 )
+
+func decodeDataURI(input string) string {
+	u, _ := dataurl.DecodeString(input)
+
+	dst := make([]byte, base64.StdEncoding.DecodedLen(len(input)))
+	base64.StdEncoding.Decode(dst, u.Data)
+
+	return string(u.Data)
+}
 
 func parseContents(input string) (string, error) {
 	r := strings.NewReader(input)
-	file, err := imagecashletter.NewReader(r).Read()
-	if err != nil {
-		return "", err
-	}
+	file, err := imagecashletter.NewReader(r,
+		imagecashletter.ReadVariableLineLengthOption(),
+		imagecashletter.ReadEbcdicEncodingOption(),
+	).Read()
 
 	var buf bytes.Buffer
-	if err := json.NewEncoder(&buf).Encode(file); err != nil {
-		return "", err
-	}
-	return buf.String(), nil
+	json.NewEncoder(&buf).Encode(file)
+
+	return buf.String(), err
+}
+
+func readAsJSON(input string) (string, error) {
+	file, err := imagecashletter.FileFromJSON([]byte(input))
+
+	pretty, _ := json.MarshalIndent(file, "", " ")
+
+	return string(pretty), err
 }
 
 func prettyJson(input string) (string, error) {
@@ -46,20 +65,36 @@ func jsonWrapper() js.Func {
 			return "Invalid no of arguments passed"
 		}
 
-		inputJSON := args[0].String()
+		input := args[0].String()
 
-		parsed, err := parseContents(inputJSON)
-		if err != nil {
-			fmt.Printf("unable to convert to json %s\n", err)
-			return err.Error()
-		}
-		pretty, err := prettyJson(parsed)
-		if err != nil {
-			fmt.Printf("unable to convert to json %s\n", err)
-			return err.Error()
+		// Decode the data URI if we're given one (file uploads)
+		if strings.HasPrefix(input, "data:") {
+			input = decodeDataURI(input)
 		}
 
-		return pretty
+		// Handle either JSON or X9-formatted contents
+		if json.Valid([]byte(input)) {
+			pretty, err := readAsJSON(input)
+			if err != nil {
+				fmt.Printf("unable to parse JSON %s\n", err)
+				return err.Error()
+			}
+			return pretty
+		} else {
+			parsed, err := parseContents(input)
+			if err != nil {
+				fmt.Printf("unable to parse contents %s\n", err)
+				return err.Error()
+			}
+
+			pretty, err := prettyJson(parsed)
+			if err != nil {
+				fmt.Printf("unable to convert to json %s\n", err)
+				return err.Error()
+			}
+
+			return pretty
+		}
 	})
 	return jsonFunc
 }
